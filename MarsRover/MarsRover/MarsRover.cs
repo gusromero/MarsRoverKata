1423079fb6165e58fd351c5c59a9a37b027d1bf2
gusromero <gusromero@gmail.com>
2018-07-07 19:33:35 +0200

    Start moving

---
+++ MarsRover/MarsRover/MarsRover.cs
@@ -0,0 +1,38 @@
+﻿namespace MarsRover
+{
+    public class MarsRover
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public string Orientation { get; set; }   //TODO: enum
+
+
+        public MarsRover()
+            : this(0, 0, "N")
+        {
+
+        }
+
+        public MarsRover(int x, int y, string orientation)
+        {
+            PositionX = x;
+            PositionY = y;
+            Orientation = orientation;
+        }
+
+        public void Command(string commands)
+        {
+            foreach (var command in commands)
+            {
+                switch (command)
+                {
+                    case 'f':
+                        PositionX++;
+                        break;
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ MarsRover/MarsRoverTests/MarsRoverTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverTests
+{
+    [TestClass]
+    public class MarsRoverTests
+    {
+        [TestMethod]
+        public void MSIsInitiallySetInOriginFacingNorth()
+        {
+            var rover = new MarsRover.MarsRover();
+
+            Assert.AreEqual(rover.PositionX, 0);
+            Assert.AreEqual(rover.PositionY, 0);
+            Assert.AreEqual(rover.Orientation, "N");
+        }
+
+        [TestMethod]
+        public void MSIsSetInTheCorrectPositionAndOrientation()
+        {
+            var rover = new MarsRover.MarsRover(3, 4, "S");
+
+            Assert.AreEqual(rover.PositionX, 3);
+            Assert.AreEqual(rover.PositionY, 4);
+            Assert.AreEqual(rover.Orientation, "S");
+        }
+
+        [TestMethod]
+        public void MSMovesForward()
+        {
+            var rover = new MarsRover.MarsRover();
+            rover.Command("f");
+
+            Assert.AreEqual(rover.PositionX, 0);
+            Assert.AreEqual(rover.PositionY, 1);
+            Assert.AreEqual(rover.Orientation, "N");
+        }
+    }
+}
